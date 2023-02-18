bc8786837b755695115941813ff1721d4146ea16
Olha Tverdokhlib <balabolya@gmail.com>
2023-02-18 10:37:14 +0100

    task-2

--- index.js
+++ index.js
@@ -20,3 +20,14 @@
 
 
 
+
+
+//2. Напишіть програму, яка запитає у користувача "Enter the number of minutes to format"
+//число (кількість хвилин) і виведе в консоль
+//рядок у форматі годин і хвилин, тобто 70 має повернути у консоль "01:10"
+
+// const input = prompt("Enter the number of minutes to format");
+// let hour = String(Math.floor(input / 60)).padStart(2, '0');
+// let minutes = String(input % 60).padStart(2, "0");
+// const time = `${hour}:${minutes}`;
+// console.log(time);
